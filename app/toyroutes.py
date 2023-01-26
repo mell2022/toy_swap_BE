6d0e824c5b7ab23d1fc01cd157b9a59d5ffa5305
Melley Gebretatios <gmelley4@gmail.com>
2023-01-26 00:38:12 -0500

    connnected api to firebase

--- app/toyroutes.py
+++ app/toyroutes.py
@@ -1,47 +1,39 @@
 # from app import db
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 from app.models.toy import Toy
 from flask import Blueprint, request, jsonify, make_response, abort
-import os, requests
+
+# Use a service account to access our firebase
+cred = credentials.Certificate('/Users/melleygebretatios/Developers/capstone/toyswap-0125-30fa05cd6bd3.json')
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 toys_bp = Blueprint("toys_bp", __name__, url_prefix="/toys")
 
-def validate_model(cls, model_id):
-    try:
-        model_id = int(model_id)
-    except:
-        abort(make_response({"message":f"{cls.__name__} {model_id} invalid"}, 400))
-
-    model = cls.query.get(model_id)
-
-    if not model:
-        abort(make_response({"message":f"{cls.__name__} {model_id} not found"}, 404))
-
-    return model
-
 @toys_bp.route("", methods=["POST"])
-def create_toy():
+def create_user():
     request_body = request.get_json()
+    doc_ref = db.collection(u'users').document(u'alovelace')
+    doc_ref.set({
+        u'first': u'Ada',
+        u'last': u'Lovelace',
+        u'born': 1965
+    })
     return make_response(request_body,201)
-    # new_toy = Toy.from_dict(request_body)
-    
-    # db.session.add(new_toy)
-    # db.session.commit()
-    
-    # return make_response(jsonify(new_toy.to_dict()), 201)
 
-@toys_bp.route("/<toy_id>", methods=["GET"])
-def get_one_toy(toy_id):
-    # fake_toy = Toy(int(toy_id),"maribel", 2.50, "telecom")
-    fake_toy = {
-        "id": "4",
-        "name": "mirabel",
-        "price":"2.50",
-        "brand": "telecom"
-    }
-    return make_response(fake_toy, 200)
-#     toy = validate_model(Toy, toy_id)
-#     # return toy.to_dict()
-#     
+@toys_bp.route("/<user_id>", methods=["GET"])
+def get_one_user(user_id):
+    users_ref = db.collection(u'users')
+    user = users_ref.document(user_id).get()
+    return jsonify(user.to_dict()), 200
 
+@toys_bp.route("", methods=["GET"])
+def get_all_toys():
+    users_ref = db.collection(u'users')
+    all_users = [doc.to_dict() for doc in users_ref.stream()]
+    return jsonify(all_users), 200
+        
 
 
